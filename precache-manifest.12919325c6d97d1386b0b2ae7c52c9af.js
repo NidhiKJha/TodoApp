f0d356fb727c0d76560b409f476224f367626b3b
Nidhi Kumari <nidhi1691996@outlook.com>
2020-03-05 19:30:39 +0530

    Updates

---
+++ precache-manifest.12919325c6d97d1386b0b2ae7c52c9af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2ccda76502ff95cd0838ba3822f3ab",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "1a3fe568f61ba2abf5ab",
+    "url": "/todo-app/static/css/main.d5c47791.chunk.css"
+  },
+  {
+    "revision": "99fb4a2159240ed658c5",
+    "url": "/todo-app/static/js/2.83ac3f23.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-app/static/js/2.83ac3f23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a3fe568f61ba2abf5ab",
+    "url": "/todo-app/static/js/main.2447710f.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  }
+]);
